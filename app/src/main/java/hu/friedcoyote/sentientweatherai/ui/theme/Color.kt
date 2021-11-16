c4fdf86c8eb4229ca106a044793a802e23ee45e5
BendeguzTajti <bendeguz9ctajti@gmail.com>
2021-11-16 23:29:14 +0100

    add color change animation

--- app/src/main/java/hu/friedcoyote/sentientweatherai/ui/theme/Color.kt
+++ app/src/main/java/hu/friedcoyote/sentientweatherai/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val DeepPurple = Color(0xFF7C4A89)
+val DeepGreen = Color(0xFF3C6157)
+val DeepBlue = Color(0xFF1B2F5C)
